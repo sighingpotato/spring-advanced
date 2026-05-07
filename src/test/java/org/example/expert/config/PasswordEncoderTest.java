2de5f90702a2d7fb173847e3c2ef0480aab1a0e1
sighingpotato <jot0202@naver.com>
2026-05-07 15:36:19 +0900

    fix : LV 4 테스트 코드 수정

--- src/test/java/org/example/expert/config/PasswordEncoderTest.java
+++ src/test/java/org/example/expert/config/PasswordEncoderTest.java
@@ -15,14 +15,14 @@ class PasswordEncoderTest {
 
     @Test
     void matches_메서드가_정상적으로_동작한다() {
-        // given
+        // given : 원본 비밀번호, 암호화 비밀번호 준비
         String rawPassword = "testPassword";
         String encodedPassword = passwordEncoder.encode(rawPassword);
 
-        // when
-        boolean matches = passwordEncoder.matches(encodedPassword, rawPassword);
+        // when : 원본과 암호화를 비교. 인자 순서를 원본, 암호화 순서로 맞춘다.
+        boolean matches = passwordEncoder.matches(rawPassword, encodedPassword);
 
-        // then
+        // then : 비교 결과가 true여야 한다.
         assertTrue(matches);
     }
 }
